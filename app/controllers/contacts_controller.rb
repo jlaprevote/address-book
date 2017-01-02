78524b0d782b4f7d5098c5513d02a5a441a6f714
jlaprevote <jacklaprevote@gmail.com>
2017-01-02 16:45:53 -0700

    added form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,23 @@
 class ContactsController < ApplicationController
+  include ContactsHelper
 
   def index
     @contacts = Contact.all
   end
-  
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+
+    redirect_to contacts_path
+  end
+
 end
--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,7 @@
 module ContactsHelper
+
+  def contact_params
+    params.require(:contact).permit(:f_name, :l_name, :address, :phone, :email)
+  end
+
 end
