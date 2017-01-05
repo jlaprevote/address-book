b3e85c43831e857aa0830618aeff8a51288390aa
jlaprevote <jacklaprevote@gmail.com>
2017-01-04 23:25:08 -0700

    add (hopefully for real) aws/paperclip fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,8 +93,7 @@ Rails.application.configure do
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION')
     },
-    :s3_host_name => "s3-us-west-2.amazonaws.com",
-    :url => ":s3_host_name"
+    :s3_host_name => "s3-#{ENV['AWS_REGION']}.amazonaws.com",
   }
 
 end
